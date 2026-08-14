2f1ea151cb04771da365ac1257851c3ebe6768f8
virajagravat <virajagravat20@gmail.com>
2026-08-14 12:45:17 +0530

    feat(profile): add profile repository

--- backend/profile-service/src/main/java/com/backend/profile_service/repository/ProfileRepository.java
+++ backend/profile-service/src/main/java/com/backend/profile_service/repository/ProfileRepository.java
@@ -1,4 +1,9 @@
 package com.backend.profile_service.repository;
 
-public interface ProfileRepository {
+import com.backend.profile_service.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile,Long> {
 }
