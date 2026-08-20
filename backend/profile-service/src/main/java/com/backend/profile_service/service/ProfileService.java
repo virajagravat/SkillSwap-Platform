2de5031124421495480e57671512f7e05091d680
virajagravat <virajagravat20@gmail.com>
2026-08-20 18:28:51 +0530

    by mistake change ProfileService.java file

---
+++ backend/profile-service/src/main/java/com/backend/profile_service/service/ProfileService.java
@@ -0,0 +1,44 @@
+package com.backend.profile_service.service;
+
+import com.backend.profile_service.entity.Profile;
+import com.backend.profile_service.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    //Search Profile by id
+    public Profile getProfileById(Long id){
+        return profileRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Profile not found with id: " + id ));
+    }
+
+    //Get All Profile
+    public List<Profile> getAllProfile(){
+        return profileRepository.findAll();
+    }
+
+    //Create Profile
+    public Profile createProfile(Profile profile){
+        return profileRepository.save(profile);
+    }
+
+    //Update Profile
+    public Profile updateProfile(Long id, String name, String profilePhoto){
+        Profile profile = profileRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Profile not found with id: " + id));
+
+        profile.setName(name);
+        profile.setProfilePhoto(profilePhoto);
+
+        return profileRepository.save(profile);
+    }
+
+
+
+}
