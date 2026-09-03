32a8f0fa695409b026ab051c1fff95a49f972fb6
virajagravat <virajagravat20@gmail.com>
2026-09-03 21:24:36 +0530

    chore(browse-skill-service): initialize service

---
+++ backend/browse-skill-service/src/main/java/com/backend/browse_skill_service/BrowseSkillServiceApplication.java
@@ -0,0 +1,15 @@
+package com.backend.browse_skill_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class BrowseSkillServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrowseSkillServiceApplication.class, args);
+	}
+
+}
---
+++ backend/browse-skill-service/src/test/java/com/backend/browse_skill_service/BrowseSkillServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.browse_skill_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrowseSkillServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
