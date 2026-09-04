58708cbf97c62b5889881d815b661f651dfb2032
virajagravat <virajagravat20@gmail.com>
2026-09-04 20:22:27 +0530

    feat: secure browse service through api gateway

---
+++ backend/browse-skill-service/src/main/java/com/backend/browse_skill_service/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.backend.browse_skill_service.config;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http) throws Exception {
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .permitAll()
+                );
+
+
+        return http.build();
+    }
+}
