b6acfc0a972d814f7fcb731f3842ce3478d2d0ef
Alkesh Bakotiya <alkeshbakotiya000@gmail.com>
2026-09-13 11:41:52 +0530

    Configure The New Database in application.properties

---
+++ backend/skill-swap-request-service/src/main/java/com/backend/skill_swap_request_service/SkillSwapRequestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.skill_swap_request_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSwapRequestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSwapRequestServiceApplication.class, args);
+	}
+
+}
